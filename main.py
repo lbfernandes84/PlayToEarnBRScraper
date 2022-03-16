417ea9e7310683b7bf79ca8853b9f9252cf17b12
Lucas Fernandes <lucas.fernandes@dataminesoftware.com>
2022-03-16 20:14:02 -0300

    Implemented the feature to retrieve all P2E games links from target page

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 from webdriver_manager.chrome import ChromeDriverManager
+
+import time
+import config
 
 class P2EGame:
 
@@ -28,10 +26,21 @@ def create_driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--ignore-ssl-errors')
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
     return webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
-def collect_data(driver, url):
-    pass
+def collect_p2e_games_list(driver, url):
+    links =[]
+    driver.get(url)
+    time.sleep(10)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    table = soup.find('table', class_="table table-bordered mainlist")
+    all_games = table.find_all('a', class_='dapp_detaillink socialscoregraph')    
+    for game in all_games:
+        links.append(game['href'])
+        print(links[-1])
+    return links
 
 if __name__ == '__main__':
-    create_driver
+    driver = create_driver()
+    collect_p2e_games_list(driver, config.MAIN_URL)
