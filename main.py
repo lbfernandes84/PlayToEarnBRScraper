dde61a6db0b45f026cad6407a9d1d933ef18148d
Lucas Fernandes <lucas.fernandes@dataminesoftware.com>
2022-03-23 18:47:08 -0300

    Implemented the data retrievement for each game presented by the links

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from urllib import response
+import string
+from urllib import request, response
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,36 +9,97 @@ import config
 
 SUCCESSFULL_REQUEST_STATUS = 200
 
+
 class P2EGame:
 
     def __init__(self):
         self.name = ''
-        self.short_description =''
+        self.status = ''
+        self.short_description = ''
         self.description = ''
         self.twitter = ''
-        self.discord_server =''
-        self.website =''
+        self.discord_server = ''
+        self.website = ''
         self.genre = []
         self.blockchain = []
         self.devices = []
         self.support_to_nft = False
         self.free_to_play = []
+        self.play_to_earn = []
+
+    def __repr__(self) -> str:
+        genre = ', '.join(self.genre)
+        blockchain = ', '.join(self.blockchain)
+        devices = ', '.join(self.devices)
+        play_to_earn = ', '.join(self.play_to_earn)
+        return f'Nome: {self.name}\n status: {self.status}\n descrição: {self.description}\n twitter: {self.twitter}\n discord: {self.discord_server}\n website: {self.website}\n genre: {genre}\n blockchain: {blockchain}\n devices: {devices}\n support nft: {self.support_to_nft}\n Free to play: {self.free_to_play}\n Play to Earn: {play_to_earn}\n'
+
 
 def connect_to_source(url):
     r = requests.get(url)
     soup = None
-    if r.status_code == SUCCESSFULL_REQUEST_STATUS:  
+    if r.status_code == SUCCESSFULL_REQUEST_STATUS:
         soup = BeautifulSoup(r.text, 'html.parser')
     return r.status_code, soup
 
+
 def collect_p2e_games_list(soup):
-    links =[]    
+    links = []
     table = soup.find('table', class_="table table-bordered mainlist")
-    all_games = table.find_all('a', class_='dapp_detaillink socialscoregraph')    
+    all_games = table.find_all('a', class_='dapp_detaillink socialscoregraph')
     for game in all_games:
         links.append(game['href'])
         # print(links[-1])
     return links
+
+
+def collect_p2e_games_details(p2e_links):
+    for link in p2e_links:
+        r = requests.get(link)
+        if r.status_code == SUCCESSFULL_REQUEST_STATUS:
+            soup = BeautifulSoup(r.text, 'html.parser')
+
+            p2egame = P2EGame()
+            # nft game name
+            div = soup.find('div', class_='headline-dapp')
+            p2egame.name = div.find('h1').string
+
+            # nft status
+            p2egame.status = soup.find('a', class_='status').string
+
+            # nft game description
+            p2egame.description = soup.find('p').string
+
+            # nft game genre
+            div = soup.find('div', class_='dapp_categories')
+            genre = div.find_all('a')
+            p2egame.genre = [a.string for a in genre]
+            print("Genre: ", p2egame.genre)
+
+            # nft game blockchain
+            div = soup.find('div', class_='dapp_platforms')
+            platforms = div.find_all('a')
+            p2egame.blockchain = [a['title'] for a in platforms]
+
+            # nft game devices
+            div = soup.find('div', class_='dapp_devices')
+            devices = div.find_all('a')
+            p2egame.devices = [a['title'] for a in devices]
+
+            # nft game details
+            div = soup.find('div', class_='dapp_nft_p2e')
+            subdiv = div.find_all('div')
+            if len(subdiv) == 3:
+                yes_or_no = subdiv[0].find('a').string
+                yes_or_no = yes_or_no.upper()
+                yes_or_no = yes_or_no.strip()
+                p2egame.support_to_nft = True if yes_or_no == "YES" else False
+
+                p2egame.free_to_play = subdiv[1].find('a').string
+                play_to_earn = subdiv[2].find_all('a')
+                p2egame.play_to_earn = [a.string for a in play_to_earn]
+            print(p2egame)
+
 
 if __name__ == '__main__':
     url = config.MAIN_URL
@@ -45,7 +107,7 @@ if __name__ == '__main__':
     all_links = []
     while url is not None:
         status, soup = connect_to_source(url)
-        all_links.extend(collect_p2e_games_list(soup)) 
+        all_links.extend(collect_p2e_games_list(soup))
         next_link = soup.find('a', class_="page-link", rel='next')
         print('status: ', status)
         print('next_link: ', next_link)
@@ -53,6 +115,8 @@ if __name__ == '__main__':
             url = next_link['href']
         else:
             url = None
-    print('Number of games: ',len(all_links))
+        break
+    collect_p2e_games_details(all_links[:10])
+    print('Number of games: ', len(all_links))
     for link in all_links:
         print(link)
